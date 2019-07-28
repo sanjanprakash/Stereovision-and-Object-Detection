a65790ee587fb2c7d862bb7ac3542c47c63f8802
Sanjan <ep14btech11007@iith.ac.in>
2019-07-28 16:53:52 -0400

    Point cloud creator to be viwed as a .ply file using MeshLab

---
+++ src/PointCloudLibrary.py
@@ -0,0 +1,28 @@
+##################################################
+# Filename: PointCloudLibrary.py
+# Function: Creates .ply file of the point cloud
+#           Can be viewed in MeshLab
+##################################################
+
+import numpy as np
+import cv2
+
+ply_header = '''ply
+format ascii 1.0
+element vertex %(vert_num)d
+property float x
+property float y
+property float z
+property uchar red
+property uchar green
+property uchar blue
+end_header
+'''
+
+def write_ply(fn, verts, colors):
+    verts = verts.reshape(-1, 3)
+    colors = colors.reshape(-1, 3)
+    verts = np.hstack([verts, colors])
+    with open(fn, 'wb') as f:
+        f.write((ply_header % dict(vert_num=len(verts))).encode('utf-8'))
+        np.savetxt(f, verts, fmt='%f %f %f %d %d %d ')
